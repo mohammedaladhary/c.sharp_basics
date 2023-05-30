bed40e9323665091ff28a51fc1310c765f4a00c7
mohammedaladhary <caladhary@gmail.com>
2023-05-30 11:29:15 +0400

    Basic informations

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Team!");
+        Console.WriteLine("Mohammed AL Adhary");
+        Console.WriteLine("Born in Muscat");
+        Console.WriteLine("In 6/4/1999");
+        Console.WriteLine("Studied Computer Science");
+    }
+}
